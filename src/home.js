65bc58502f037860779d061c797e756fddaa3413
trantuananhr1 <118260451+trantuananhr1@users.noreply.github.com>
2025-06-29 13:27:52 +0700

    Create home.js

---
+++ src/home.js
@@ -0,0 +1,9 @@
+function execute() {
+  return Response.success([
+    {
+      title: "Latest Updates",
+      input: "https://www.novelupdates.com/",
+      script: "homecontent.js"
+    }
+  ]);
+}
