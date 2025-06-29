136cb2b4bded63e799ad2218732dbfd677f32c13
trantuananhr1 <118260451+trantuananhr1@users.noreply.github.com>
2025-06-29 13:39:05 +0700

    Update search.js

--- src/search.js
+++ src/search.js
@@ -1,16 +1,22 @@
 function execute(key, page) {
-  const url = `https://www.novelupdates.com/?s=${encodeURIComponent(key)}&post_type=seriesplans`;
-  const doc = fetch(url).html();
-  const list = [];
+    let searchUrl = "https://www.novelupdates.com/?s=" + encodeURIComponent(key);
+    let doc = fetch(searchUrl).html();
+    let list = [];
 
-  doc.select(".search_main_box_nu").forEach(item => {
-    list.push({
-      name: item.select(".search_title a").text(),
-      link: item.select(".search_title a").attr("href"),
-      cover: item.select("img").attr("src"),
-      description: item.select(".search_intro").text()
+    doc.select("div.search_main_box_nu").forEach(card => {
+        let name = card.select("div.search_title a").text();
+        let link = card.select("div.search_title a").attr("href");
+        let cover = card.select("img").attr("src");
+        let desc = card.select("div.search_intro").text();
+
+        list.push({
+            name: name,
+            link: link,
+            cover: cover,
+            description: desc,
+            host: "www.novelupdates.com"
+        });
     });
-  });
 
-  return Response.success(list);
+    return Response.success(list);
 }
